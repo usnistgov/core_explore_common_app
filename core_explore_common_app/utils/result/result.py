4d65e9e98acc4a89188e18174b50f3f1a3b866cd
Guillaume Sousa <guillaume.sousa@nist.gov>
2017-05-23 16:58:02 -0400

    Avoid multiple api calls to get name of template
    Change-Id: I6844f0bae3976fe547310e2d18af134b1259b620

--- core_explore_common_app/utils/result/result.py
+++ core_explore_common_app/utils/result/result.py
@@ -1,6 +1,5 @@
 """Explore Common result utils
 """
-from core_main_app.components.version_manager import api as version_manager_api
 from core_explore_common_app.components.result.models import Result
 from core_explore_common_app.rest.result.serializers import ResultSerializer, ResultBaseSerializer
 import json
@@ -16,12 +15,9 @@ def get_template_info(template, include_template_id=True):
         Template information.
 
     """
-    version_manager = version_manager_api.get_from_version(template)
-    version_number = version_manager_api.get_version_number(version_manager, template.id)
-
     # Here the id need to be set anyway because is expected by the serializer
     return_value = {'id': template.id if include_template_id else '',
-                    'name': "{0} (Version {1})".format(version_manager.title, version_number),
+                    'name': template.display_name,
                     'hash': template.hash}
 
     return return_value
