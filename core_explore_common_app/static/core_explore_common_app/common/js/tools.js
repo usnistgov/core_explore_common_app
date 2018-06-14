34c82073d25b14955ec24026706a2edf7e9d3612
Pierre Francois RIGODIAT <pierre-francois.rigodiat@nist.gov>
2018-06-14 13:22:18 -0400

    feat: Add cachedScript function
    Change-Id: Ife0f9dcc10e16624f6eb0d6c9110537e2bf0e7a3

--- core_explore_common_app/static/core_explore_common_app/common/js/tools.js
+++ core_explore_common_app/static/core_explore_common_app/common/js/tools.js
@@ -10,3 +10,22 @@ var onjQueryReady = function(method) {
         setTimeout(function() { onjQueryReady(method) } , 50);
     }
 };
+
+/**
+ * Get script in param thanks to its url.
+ * Use a cache to avoid getting several time the same script.
+ * @param url
+ * @param options
+ */
+cachedScript = function( url, options ) {
+  // Allow user to set any option except for dataType, cache, and url
+  options = $.extend( options || {}, {
+    dataType: "script",
+    cache: true,
+    url: url
+  });
+
+  // Use $.ajax() since it is more flexible than $.getScript
+  // Return the jqXHR object so we can chain callbacks
+  return $.ajax( options );
+};
