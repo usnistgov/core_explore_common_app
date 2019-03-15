12b276578bb77cc7bb99d0277f736a59d2b6d779
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-03-14 22:18:13 -0400

    fix: core 1.0.1 sets kombu version to avoid broken celery dependency
    Change-Id: I7f95a5b6f44e35df434b768d0617215349d4ddf2

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ required = [req_link(r) if r.startswith("https://") else r for r in required]
 
 setup(
     name="core_explore_common_app",
-    version="1.0.0",
+    version="1.0.1",
     description="Base exploration function for the curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
