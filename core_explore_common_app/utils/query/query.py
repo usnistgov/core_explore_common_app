8f856d28dbc1d3f5fcd04ea0978af7445c0fbea1
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-10-30 10:15:56 -0400

    feat: upgrade django 2.2
    Change-Id: I09aef31f3aaa5804974690a75f297775641df1af

--- core_explore_common_app/utils/query/query.py
+++ core_explore_common_app/utils/query/query.py
@@ -3,7 +3,7 @@
 import json
 import urllib
 
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from requests import ConnectionError
 
 from core_explore_common_app.commons.exceptions import ExploreRequestError
