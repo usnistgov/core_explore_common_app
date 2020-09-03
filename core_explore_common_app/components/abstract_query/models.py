aac289d6f765fdd34504511300d6375e082c7765
Philippe Dessauw <philippe.dessauw@nist.gov>
2020-09-03 11:45:10 -0400

    fix: set capabilities as optional
    Change-Id: Id33facce3b361737fac9345250ec0c73025a4b5c

--- core_explore_common_app/components/abstract_query/models.py
+++ core_explore_common_app/components/abstract_query/models.py
@@ -23,7 +23,7 @@ class DataSource(EmbeddedDocument):
     query_options = fields.DictField(blank=True)
     authentication = fields.EmbeddedDocumentField(Authentication)
     order_by_field = fields.StringField(blank=True, default="")
-    capabilities = fields.DictField(blank=False)
+    capabilities = fields.DictField(blank=True)
 
 
 class AbstractQuery(Document):
