be485b4d2dc3b92a4cba5431cb4c65d07e61b4f5
Guillaume Sousa <guillaume.sousa@nist.gov>
2022-12-14 15:21:24 +0000

    chore: upgrade 2.2.0
    Change-Id: I260f01d0069bf74170bb34355d7600fe1d6fdeb9

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ required = [req_link(r) if r.startswith("https://") else r for r in required]
 
 setup(
     name="core_explore_common_app",
-    version="2.1.0",
+    version="2.2.0",
     description="Base exploration function for the curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
