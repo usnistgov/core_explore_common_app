1eebac5fac21e6ab19cf5943c367e2cece1d7b9a
Philippe Dessauw <philippe.dessauw@nist.gov>
2023-01-17 14:07:33 -0500

    fix: use tz aware datetimes
    Change-Id: I7af18d2180cf2e8cafb7b6a5bb14afae14aece75

--- core_explore_common_app/tasks.py
+++ core_explore_common_app/tasks.py
@@ -1,13 +1,12 @@
 """ Explore Common App tasks
 """
 import logging
-from datetime import timedelta
 
 from celery import shared_task
-from django.utils import timezone
 
 from core_explore_common_app.settings import QUERIES_MAX_DAYS_IN_DATABASE
 from core_explore_common_app.system.api import get_all_queries
+from core_main_app.utils.datetime import datetime_now, datetime_timedelta
 
 logger = logging.getLogger(__name__)
 
@@ -25,7 +24,8 @@ def delete_old_queries():
             query
             for query in get_all_queries()
             if query.creation_date
-            < timezone.now() - timedelta(days=QUERIES_MAX_DAYS_IN_DATABASE)
+            < datetime_now()
+            - datetime_timedelta(days=QUERIES_MAX_DAYS_IN_DATABASE)
         ]
         # remove old queries from database
         for query in old_queries:
