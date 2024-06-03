887dbaabe73aa8e94b4eeb13289a0442be8569f7
Guillaume-sousa <guillaume.sousa@nist.gov>
2024-06-03 16:55:18 -0400

    feat: render data with html template
    Change-Id: I88f894a613ed4c88d5b19f896a451053c404d0aa

---
+++ core_explore_common_app/templatetags/result_to_html.py
@@ -0,0 +1,76 @@
+""" Convert result to HTML
+"""
+import logging
+
+from django import template as django_template
+
+from core_explore_common_app.components.result.models import Result
+from core_main_app.templatetags.data_to_html import (
+    _render_data_html,
+    _get_template_html_rendering,
+    _data_content_to_dict,
+)
+
+register = django_template.Library()
+logger = logging.getLogger(__name__)
+
+
+@register.simple_tag(name="result_list_html")
+def result_list_html(result):
+    """result_list_html
+
+    Args:
+        result:
+
+    Returns:
+
+    """
+    # if result is a Result object
+    if isinstance(result, Result):
+        # get template info
+        template_info = result.template_info
+        # get result content
+        result_content = result.content
+    # if result is a dict
+    elif isinstance(result, dict):
+        # get template info
+        template_info = result.get("template_info", None)
+        # get result content
+        result_content = result["content"]
+    # if result is unknown type, return None
+    else:
+        return None
+
+    # if template info not provided, return None
+    if not template_info:
+        return None
+
+    # get template id from template info
+    template_id = template_info.get("id", None)
+    # get template hash from template info
+    template_hash = template_info.get("hash", None)
+    # get template format from template info
+    template_format = template_info.get("format", None)
+
+    try:
+        # get detail html rendering for this template
+        template_html_rendering = _get_template_html_rendering(
+            template_id=template_id,
+            template_hash=template_hash,
+            rendering_type="list",
+        )
+        # if template html rendering not set, return None
+        if not template_html_rendering:
+            return None
+        # if result is not a dict
+        if not isinstance(result_content, dict):
+            result_content = _data_content_to_dict(
+                result_content, template_format
+            )
+        # render result content with list rendering template
+        return _render_data_html(template_html_rendering, result_content)
+    except Exception as e:
+        logger.error(
+            "An error occurred while rendering data to html: " + str(e)
+        )
+        return None
---
+++ tests/templatetags/test_result_to_html.py
@@ -0,0 +1,212 @@
+""" Unit tests of data_to_html templatetag
+"""
+from unittest.case import TestCase
+from unittest.mock import patch, MagicMock
+
+from core_explore_common_app.components.result.models import Result
+from core_main_app.commons.exceptions import DoesNotExist
+from core_explore_common_app.templatetags.result_to_html import (
+    result_list_html,
+)
+
+
+class TestResultToHtml(TestCase):
+    """TestResultToHtml"""
+
+    def test_result_html_with_none_returns_none(self):
+        """test_result_html_with_none_returns_none
+
+        Returns:
+
+        """
+        result = result_list_html(None)
+        self.assertIsNone(result)
+
+    @patch(
+        "core_main_app.components.template_html_rendering.api.get_by_template_id"
+    )
+    def test_result_html_without_template_html_rendering_returns_none(
+        self, mock_get_by_template_id
+    ):
+        """test_result_html_without_template_html_rendering_returns_none
+
+        Returns:
+
+        """
+        mock_get_by_template_id.side_effect = DoesNotExist("Error")
+        mock_data = {
+            "content": {"value": "test"},
+            "template_info": {"format": "JSON", "id": 1},
+        }
+        result = result_list_html(mock_data)
+        self.assertIsNone(result)
+
+    @patch(
+        "core_main_app.components.template_html_rendering.api.get_by_template_id"
+    )
+    def test_result_html_without_detail_rendering_returns_none(
+        self, mock_get_by_template_id
+    ):
+        """test_result_html_without_detail_rendering_returns_none
+
+        Returns:
+
+        """
+        mock_template_html_rendering = MagicMock()
+        mock_template_html_rendering.list_rendering = None
+        mock_get_by_template_id.return_value = mock_template_html_rendering
+        mock_result = {
+            "content": {"value": "test"},
+            "template_info": {"format": "JSON", "id": 1},
+        }
+        result = result_list_html(mock_result)
+        self.assertIsNone(result)
+
+    @patch(
+        "core_main_app.components.template_html_rendering.api.get_by_template_id"
+    )
+    def test_result_html_with_dict_returns_html(self, mock_get_by_template_id):
+        """test_result_html_with_dict_returns_html
+
+        Returns:
+
+        """
+        mock_template_html_rendering = MagicMock()
+        mock_template_html_rendering.list_rendering = "{{dict_content.value}}"
+        mock_get_by_template_id.return_value = mock_template_html_rendering
+        mock_result = {
+            "content": {"value": "test"},
+            "template_info": {"format": "JSON", "id": 1},
+        }
+        result = result_list_html(mock_result)
+        self.assertEqual(result, "test")
+
+    @patch(
+        "core_main_app.components.template_html_rendering.api.get_by_template_id"
+    )
+    def test_result_html_with_string_dict_returns_html(
+        self, mock_get_by_template_id
+    ):
+        """test_result_html_with_dict_returns_html
+
+        Returns:
+
+        """
+        mock_template_html_rendering = MagicMock()
+        mock_template_html_rendering.list_rendering = "{{dict_content.value}}"
+        mock_get_by_template_id.return_value = mock_template_html_rendering
+        mock_result = {
+            "content": '{"value": "test"}',
+            "template_info": {"format": "JSON", "id": 1},
+        }
+        result = result_list_html(mock_result)
+        self.assertEqual(result, "test")
+
+    @patch(
+        "core_main_app.components.template_html_rendering.api.get_by_template_id"
+    )
+    def test_result_html_returns_html(self, mock_get_by_template_id):
+        """test_result_html_returns_html
+
+        Returns:
+
+        """
+        mock_template_html_rendering = MagicMock()
+        mock_template_html_rendering.list_rendering = "{{dict_content.value}}"
+        mock_get_by_template_id.return_value = mock_template_html_rendering
+        mock_result = Result(
+            content={"value": "test"},
+            template_info={"format": "JSON", "id": 1},
+        )
+        result = result_list_html(mock_result)
+        self.assertEqual(result, "test")
+
+    @patch(
+        "core_main_app.components.template_html_rendering.api.get_by_template_id"
+    )
+    def test_result_html_with_xml_returns_html(self, mock_get_by_template_id):
+        """test_result_html_with_xml_returns_html
+
+        Returns:
+
+        """
+        mock_template_html_rendering = MagicMock()
+        mock_template_html_rendering.list_rendering = "{{dict_content.value}}"
+        mock_get_by_template_id.return_value = mock_template_html_rendering
+        mock_result = {
+            "content": "<value>test</value>",
+            "template_info": {"format": "XSD", "id": 1},
+        }
+        result = result_list_html(mock_result)
+        self.assertEqual(result, "test")
+
+    @patch(
+        "core_main_app.components.template_html_rendering.api.get_by_template_id"
+    )
+    def test_result_html_with_unknown_format_returns_none(
+        self, mock_get_by_template_id
+    ):
+        """test_result_html_with_unknown_format_returns_none
+
+        Returns:
+
+        """
+        mock_template_html_rendering = MagicMock()
+        mock_template_html_rendering.list_rendering = "{{dict_content.value}}"
+        mock_get_by_template_id.return_value = mock_template_html_rendering
+        mock_result = {
+            "content": "value: test",
+            "template_info": {"format": "YAML", "id": 1},
+        }
+        result = result_list_html(mock_result)
+        self.assertIsNone(result)
+
+    @patch(
+        "core_main_app.components.template_html_rendering.api.get_by_template_hash"
+    )
+    def test_result_html_get_by_hash_with_dict_returns_html(
+        self, mock_get_by_template_hash
+    ):
+        """test_result_html_get_by_hash_with_dict_returns_html
+
+        Returns:
+
+        """
+        mock_template_html_rendering = MagicMock()
+        mock_template_html_rendering.list_rendering = "{{dict_content.value}}"
+        mock_get_by_template_hash.return_value = mock_template_html_rendering
+        mock_result = {
+            "content": {"value": "test"},
+            "template_info": {"format": "JSON", "hash": "abcd"},
+        }
+        result = result_list_html(mock_result)
+        self.assertEqual(result, "test")
+
+    def test_result_html_returns_none_if_id_or_hash_missing(
+        self,
+    ):
+        """test_result_html_returns_none_if_id_or_hash_missing
+
+        Returns:
+
+        """
+        mock_result = {
+            "content": {"value": "test"},
+            "template_info": {"format": "JSON"},
+        }
+        result = result_list_html(mock_result)
+        self.assertIsNone(result)
+
+    def test_result_html_returns_none_if_template_info_missing(
+        self,
+    ):
+        """test_result_html_returns_none_if_template_info_missing
+
+        Returns:
+
+        """
+        mock_result = {
+            "content": {"value": "test"},
+        }
+        result = result_list_html(mock_result)
+        self.assertIsNone(result)
